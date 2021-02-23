e41d5b2bdc233c1017c54526ec0030865ca14eab
Septo Adrianto Azhar <74102464+yuzuru-otonashi@users.noreply.github.com>
2021-02-23 22:00:45 +0700

    Update hu_tao.py

--- hu_tao.py
+++ hu_tao.py
@@ -26,6 +26,10 @@ async def on_message(message):
     if any(word in message.content for word in Hu_tao):
         await message.channel.send('Wangy wangy~ :heart:')
         await bot.process_commands(message)
+        
+    if any(word in message.content for word in Hu_tao):
+        await message.channel.send('Apa sayang?')
+        await bot.process_commands(message)
 
 # keep_alive()
 bot.run(os.getenv('TOKEN'))
